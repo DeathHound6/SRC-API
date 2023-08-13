6669ad959624a82a8d19451e255fc3bcf744a50a
DeathHound <acraig176@hotmail.com>
2023-08-13 10:48:05 +0100

    Rename interface and add Leaderboard class

--- src/Leaderboards/Leaderboard.ts
+++ src/Leaderboards/Leaderboard.ts
@@ -1,7 +1,36 @@
-import { RunPlace, RunTime } from "../Runs/Run";
+import { IRunPlace, RunTime } from "../Runs/Run";
 import { Link } from "../util/API";
 
-export interface Leaderboard {
+export class Leaderboard implements ILeaderboard {
+    readonly weblink: string;
+    readonly game: string;
+    readonly category: string;
+    readonly level: string | null;
+    readonly platform: string | null;
+    readonly region: string | null;
+    readonly emulators: boolean | null;
+    readonly 'video-only': boolean;
+    readonly timing: RunTime;
+    readonly values: { [id: string]: string; };
+    readonly runs: IRunPlace[];
+    readonly links: Link[];
+
+    constructor(data: ILeaderboard) {
+        this.weblink = data.weblink;
+        this.game = data.game;
+        this.category = data.category;
+        this.level = data.level;
+        this.platform = data.platform;
+        this.region = data.region;
+        this.emulators = data.emulators;
+        this.timing = data.timing;
+        this.values = data.values;
+        this.runs = data.runs;
+        this.links = data.links;
+    }
+}
+
+export interface ILeaderboard {
     weblink: string;
     game: string;
     category: string;
@@ -14,6 +43,6 @@ export interface Leaderboard {
     values: {
         [id: string]: string;
     };
-    runs: RunPlace[];
+    runs: IRunPlace[];
     links: Link[];
 }
