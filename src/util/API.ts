997f7fe7a281cbad13b7587c65a689abf4d61549
DeathHound <acraig176@hotmail.com>
2023-08-13 10:44:55 +0100

    Add orderby enum

--- src/util/API.ts
+++ src/util/API.ts
@@ -13,3 +13,16 @@ export enum Direction {
     DESC = 'desc',
     ASC = 'asc'
 }
+
+export enum Orderby {
+    GAME = "game",
+    CATEGORY = "category",
+    LEVEL = "level",
+    PLATFORM = "platform",
+    REGION = "region",
+    EMULATED = "emulated",
+    DATE = "date",
+    SUBMITTED = "submitted",
+    STATUS = "status",
+    VERIFYDATE = "verify-date"
+}
